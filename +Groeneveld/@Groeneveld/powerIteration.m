a4abc0d9b37db5674e6551a850b82582c328ef43
RWMorse <rmorse@wisc.edu>
2023-07-31 22:05:17 -0500

    feat(powerIteration)
    The basic algorithm skeleton for the power iteration.
    
     This still needs optimization. Currently its kind of slow. But only 5-10 power iterations are needed on average.
    
    The convergence criteria is an outlet film thickness.

--- +Groeneveld/@Groeneveld/powerIteration.m
+++ +Groeneveld/@Groeneveld/powerIteration.m
@@ -3,36 +3,59 @@ function powerIteration(obj)
 %criteria.
 % Detailed description goes here.
 
+gr = obj;
+
+%settings
+delta_out = 25e-6; 
+maxitr = 50;
+
 
 % Get input options list template
 opts = obj.inputOptions();
 
-% Setup base options
-opts.model.MOMENTFILM = 'ALGEBRAIC';
-% ...etc etc etc
+    
+%   ID cannot be overridden
+    Inputs.Model().listInputProperties("exclude",{'ID'})
 
-% Run 
+%   Create the structure for custom properties
+    opts = gr.inputOptions();
+
+    opts.model.MOMENTFILM = 'ALGEBRAIC';    % change the MOMENTFILM model to ALGEBRAIC
+    opts.model.POSFILM = 0; 
+    opts.boundaryConditions.TIME = [0 3];   % change the time steps to [0 3]
+    opts.boundaryConditions.POWER =   (gr.dataset(gr.entryID,:).CHF) * 1000 * (gr.dataset(gr.entryID,:).HeatedLength) *pi*(gr.dataset(gr.entryID,:).TubeDiameter) ;
+%   Finally, make input files with opts
+    gr.makeInputFiles(opts);
+
+
+% Run case
 gr.runCase();
 
-% Retrieve results
-results = obj.results;
 
-% -> while <some conditions>
-% 
-% Iterate options
-opts.boundaryConditions.POWER = gr.results.inputSet.bc.POWER * 0.01;
+%Iterating power
+    itr = 1;
+    while (itr<maxitr)
+        delta_itr = gr.results.film(gr.results.NTIME).THICK(end);
+         if abs(delta_itr)<delta_out
+            break
+        end
+        % Check mass flux per unit perimeter
+        WLout = gr.results.film(gr.results.NTIME).WL(end);
+       
+        % Calculate a change in power
+        Powerchange = sign(delta_itr)*max(0.01,abs(WLout)/10); % 1% power increase corresponding to 0.1 kg/m-s    
+        
+        %update the power 
+        opts.boundaryConditions.POWER = (gr.results.inputSet.bc.POWER) * (1 + Powerchange) ;
+       
+        % Update the input files
+        gr.makeInputFiles(opts);
+        
+        %Run the new case
+        gr.runCase();
+        itr = itr+1;
+    end
 
-% Run 
-% -> gr.runCase();
-
-% Retrieve results
-% -> results = obj.results;
-
-% Determine if conditions are met
-% TODO
-
-% Exit loop
-% -> end
 
 
 % Currently not implemented 
